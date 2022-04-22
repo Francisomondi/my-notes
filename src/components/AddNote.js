ef9685cb28e3c2dbb22ddef0abf0bdf65c306323
francis omondi <francisomondi17@gmail.com>
2022-04-23 00:06:38 +0300

    display added note

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import { useState } from 'react'
 
+const AddNote = ({handleAddNote}) => {
 
-const AddNote = () => {
+    const [noteText, setNoteText] = useState('')
+
+    const handleChange = (event) => {
+        setNoteText(event.target.value)
+    }
+    const handleSaveClick =()=>{
+        handleAddNote(noteText)
+    }     
+
     return (
         <div className="note new">
             <textarea rows="8"
                 cols="10"
-                placeholder="type to add a note"></textarea>
+                placeholder="type to add a note"
+                value={noteText}
+                onChange={handleChange}>
+            </textarea>
             <div className="note-footer">
                 <small> 200 remaining</small>
-                <button className="save"> Save</button>
+                 <button className="save" onClick ={handleSaveClick}> Save</button>
             </div>
 
         </div>

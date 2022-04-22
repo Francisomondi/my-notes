e0190db1324696b15c1ce7a5e95f226363e73158
francis omondi <francisomondi17@gmail.com>
2022-04-23 01:00:06 +0300

    validation before saving a new note, no white space and  no empty strings

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,28 +1,31 @@
 import React from 'react'
 import { useState } from 'react'
 
-const AddNote = ({handleAddNote}) => {
+const AddNote = ({ handleAddNote }) => {
 
     const [noteText, setNoteText] = useState('')
 
     const handleChange = (event) => {
         setNoteText(event.target.value)
     }
-    const handleSaveClick =()=>{
-        handleAddNote(noteText)
-    }     
+    const handleSaveClick = () => {
+        if (noteText.trim().length > 0) {
+            handleAddNote(noteText)
+        }
+
+    }
 
     return (
         <div className="note new">
             <textarea rows="8"
                 cols="10"
                 placeholder="type to add a note"
-                value={noteText}
+                alue={noteText}
                 onChange={handleChange}>
             </textarea>
-            <div className="note-footer">
+            < div className="note-footer">
                 <small> 200 remaining</small>
-                 <button className="save" onClick ={handleSaveClick}> Save</button>
+                <button className="save" onClick={handleSaveClick}> Save</button>
             </div>
 
         </div>

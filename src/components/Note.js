7e84f93cfcd5fe87bdaf181e20b0c328c90b17cd
francis omondi <francisomondi17@gmail.com>
2022-04-22 11:13:06 +0300

    clean up on css

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,7 +3,7 @@ import { MdDeleteForever } from 'react-icons/md';
 const Note = () => {
     return (
         <div className="note">
-            <span>this is going to be my first note</span>
+            <span>This is going to be my first note</span>
             <div className="note-footer">
                 <small>13th february 2022</small>
                 <MdDeleteForever className="delete-icon" size="1.3em" />
